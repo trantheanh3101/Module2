0523b0d49c5d539564b1a05a88edcc83670ba11c
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-08 17:26:01 +0700

    wed

---
+++ src/loopJava/demoLoop.java
@@ -0,0 +1,23 @@
+package loopJava;
+
+public class demoLoop {
+    public static void main(String[] args) {
+
+//        for (int i = 0; i < 5; i++) {
+//            System.out.println("Giá trị của i là: " + i);
+//        }
+
+        int i = 0;
+        while (i < 5) {
+            System.out.println("Giá trị của i là: " + i);
+            i++;
+        }
+//
+//        int i = 0;
+//        do {
+//            System.out.println("Giá trị của i là: " + i);
+//            i++;
+//        } while (i < 5);
+
+    }
+}
