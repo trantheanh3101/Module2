7db68f1a70b6de01bf0de31eae2b77075c068133
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-08 22:29:17 +0700

    pc

---
+++ src/IntroductionToJava/transferMoney.java
@@ -0,0 +1,19 @@
+package IntroductionToJava;
+
+import java.util.Scanner;
+
+public class transferMoney {
+    public static void main(String[] args) {
+
+        float rate = 23000f;
+        float usd;
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("fill usd transfer: ");
+        usd = scanner.nextFloat();
+
+        float vnd = usd * rate ;
+        System.out.println("VND : " + vnd);
+    }
+}
