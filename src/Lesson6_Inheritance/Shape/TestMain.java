f69f3e81a53c1203923c340d6da550b03665e2c0
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-14 08:46:32 +0700

    tuesday

---
+++ src/Lesson6_Inheritance/Shape/TestMain.java
@@ -0,0 +1,8 @@
+package Lesson6_Inheritance.Shape;
+
+public class TestMain {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+    }
+}
