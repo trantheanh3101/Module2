3ed0512d475f370b3f75a7feece9e3fe453c30e7
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-16 15:47:55 +0700

    tuesday-Shape-Triangle

---
+++ src/MiniTest/MainBook.java
@@ -0,0 +1,44 @@
+package MiniTest;
+
+public class MainBook {
+    public static void main(String[] args) {
+        ProgrammingBook[] programmingBooks = new ProgrammingBook[3];
+        programmingBooks[0] = new ProgrammingBook(1,"python basic",100,"Thế Anh","PYTHON","API");
+        programmingBooks[1] = new ProgrammingBook(2,"java basic beginer",70,"Kiều Anh","JAVA","Struts");
+        programmingBooks[2] = new ProgrammingBook(3,"OOP in java",150,"John","JAVA","Hibernate");
+
+        FictionBook[] fictionBooks = new FictionBook[3];
+        fictionBooks[0] = new FictionBook(4, "The Hunger Games", 20, "Suzanne Collins", "Viễn tưởng 1");
+        fictionBooks[1] = new FictionBook(5, "Harry Potter and the Sorcerer's Stone", 50, "J.K. Rowling", "Fantasy");
+        fictionBooks[2] = new FictionBook(6, "1984", 300, "George Orwell", "Viễn tưởng 1");
+
+        double totalPriceBook = 0;
+        for (Book book : programmingBooks) {
+            totalPriceBook += book.getAmount();
+        }
+        for (Book book : fictionBooks) {
+            totalPriceBook += book.getAmount();
+        }
+        System.out.println("Total money = " + totalPriceBook);
+
+
+        int countPB = 0;
+        for (ProgrammingBook PBook : programmingBooks){
+            if (PBook.getLanguage().equals("JAVA"))
+                countPB++;
+        }
+        System.out.println("số sách ProgrammingBook có language là JAVA : " + countPB + " cuốn");
+
+        int countFB = 0;
+        int countFBPrice = 0;
+        for (FictionBook FBook : fictionBooks){
+            if (FBook.getCategory().equals("Viễn tưởng 1"))
+                countFB++;
+            if (FBook.getPrice()<100)
+                countFBPrice ++;
+        }
+        System.out.println("Số sách Fiction có category là “Viễn tưởng 1” : " + countFB + " cuốn");
+        System.out.println("Số sách Fiction có giá < 100 : " + countFBPrice + " cuốn");
+
+    }
+}
---
+++ src/MiniTest/MainTest_C2.java
@@ -0,0 +1,51 @@
+package MiniTest;
+
+public class MainTest_C2 {
+    public static void main(String[] args) {
+        Book[] Books = new Book[6];
+        Books[0] = new ProgrammingBook(1,"python basic",100,"Thế Anh","PYTHON","API");
+        Books[1] = new ProgrammingBook(2,"java basic beginer",70,"Kiều Anh","JAVA","Struts");
+        Books[2] = new ProgrammingBook(3,"OOP in java",150,"John","JAVA","Hibernate");
+
+//        FictionBook[] fictionBooks = new FictionBook[3];
+        Books[3] = new FictionBook(4, "The Hunger Games", 20, "Suzanne Collins", "Viễn tưởng 1");
+        Books[4] = new FictionBook(5, "Harry Potter and the Sorcerer's Stone", 50, "J.K. Rowling", "Fantasy");
+        Books[5] = new FictionBook(6, "1984", 300, "George Orwell", "Viễn tưởng 1");
+
+        double totalPriceBook = 0;
+        int countPB = 0;
+        int countFB = 0;
+        int countFBPrice = 0;
+        for (Book book : Books) {
+            if (book instanceof ProgrammingBook){
+                totalPriceBook += book.getAmount();
+                if(((ProgrammingBook) book).getLanguage().equals("JAVA"))
+                    countPB++;
+                }
+            if (book instanceof FictionBook) {
+                totalPriceBook += book.getAmount();
+                if (((FictionBook) book).getCategory().equals("Viễn tưởng 1"))
+                    countFB++;
+                if (book.getPrice()<100)
+                    countFBPrice ++;
+            }
+        }
+        System.out.println("Total money = " + totalPriceBook);
+        System.out.println("số sách ProgrammingBook có language là JAVA : " + countPB + " cuốn");
+        System.out.println("Số sách Fiction có category là “Viễn tưởng 1” : " + countFB + " cuốn");
+        System.out.println("Số sách Fiction có giá < 100 : " + countFBPrice + " cuốn");
+
+//        int countPB = 0;
+//        int countFB = 0;
+//        for (Book book : Books) {
+//            if (book instanceof ProgrammingBook)
+//                if(((ProgrammingBook) book).getLanguage().equals("JAVA"))
+//                    countPB++;
+//            if (book instanceof FictionBook)
+//                if (((FictionBook) book).getCategory().equals("Viễn tưởng 1"))
+//                    countFB++;
+//        }
+//        System.out.println("số sách ProgrammingBook có language là JAVA : " + countPB + " cuốn");
+//        System.out.println("Số sách Fiction có category là “Viễn tưởng 1” : " + countFB + " cuốn");
+    }
+}
