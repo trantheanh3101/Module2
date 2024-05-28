c427026c73d163ae547128ebd29da1c6eecd2647
trantheanh3101 <trantheanhk11@gmail.com>
2024-05-28 21:31:05 +0700

    lt

--- src/Lesson15_Exception/Triangle.java
+++ src/Lesson15_Exception/Triangle.java
@@ -7,16 +7,16 @@ public class Triangle {
 
     public Triangle(double side1, double side2, double side3) throws IllegalTriangleException {
 
-        this.side1 = side1;
-        this.side2 = side2;
-        this.side3 = side3;
-
         if (side1 <= 0 || side2 <= 0 || side3 <= 0) {
             throw new IllegalTriangleException("Các cạnh của tam giác phải là số dương.");
         }
         if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
             throw new IllegalTriangleException("Tổng của hai cạnh bất kỳ phải lớn hơn cạnh còn lại.");
         }
+
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
     }
 
     @Override
--- src/lesson16_IO_File/test.java
+++ src/lesson16_IO_File/test.java
@@ -1,17 +1,22 @@
 package lesson16_IO_File;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class test {
-    public static void main(String[] args) {
-        java.io.File file = new java.io.File("image/us.gif");
-        System.out.println("Does it exist? " + file.exists());
-        System.out.println("The file has " + file.length() + " bytes");
-        System.out.println("Can it be read? " + file.canRead());
-        System.out.println("Can it be written? " + file.canWrite());
-        System.out.println("Is it a directory? " + file.isDirectory());
-        System.out.println("Is it a file? " + file.isFile());
-        System.out.println("Is it absolute? " + file.isAbsolute());
-        System.out.println("Is it hidden? " + file.isHidden());
-        System.out.println("Absolute path is " + file.getAbsolutePath());
-        System.out.println("Last modified on " + new java.util.Date(file.lastModified()));
+    public static void main(String[] args) throws IOException {
+        FileReader reader = new FileReader("C:\\Users\\admin\\Desktop\\TheanhCode\\Module2\\src\\lesson16_IO_File\\data.txt");
+
+        // Tạo BufferedReader để đọc dữ liệu theo từng dòng
+        BufferedReader br = new BufferedReader(reader);
+
+        // Đọc từng dòng trong tập tin
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+        reader.close();
     }
 }
