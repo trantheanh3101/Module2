7d31588bb80af8e15eb692154286c35550d3d6e0
trantheanh3101 <trantheanhk11@gmail.com>
2024-06-01 10:38:39 +0700

    ltt

---
+++ src/Lesson19_String/test.java
@@ -0,0 +1,14 @@
+package Lesson19_String;
+
+public class test {
+    public static void main(String[] args) {
+        String str1 = "shareprogrammimg";
+        String str2 = "shareprogrammimg";
+
+        String str12 = new String("shareprogrammimg");
+        String str22 = new String("shareprogramimg");
+
+        int a =50;
+        int b = 60;
+    }
+}
