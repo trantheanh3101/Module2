80145acfb06f31eeaadaffc21482100c8778cbe5
trantheanh3101 <trantheanhk11@gmail.com>
2024-06-06 23:40:34 +0700

    pc

---
+++ src/MVC_Exercise/a.java
@@ -0,0 +1,64 @@
+package MVC_Exercise;
+
+//                 +---------------------------+
+//                 |     Hệ Thống Thư Viện     |
+//                 +---------------------------+
+//                 |                           |
+//                 |  +--------------------+   |
+//                 |  |  Đăng ký sinh viên  |<-----+
+//                 |  +--------------------+   |   |
+//                 |                           |   |
+//                 |  +--------------------+   |   |
+//                 |  |   Mượn sách        |<---------+
+//                 |  +--------------------+   |   |   |
+//                 |                           |   |   |
+//                 |  +--------------------+   |   |   |
+//                 |  |   Trả sách         |<---------+   |
+//                 |  +--------------------+   |   |   |   |
+//                 |                           |   |   |   |
+//                 |  +--------------------+   |   |   |   |
+//                 |  |  Thêm phiếu mượn   |<-----------------+
+//                 |  +--------------------+   |   |   |   |   |
+//                 |                           |   |   |   |   |
+//                 |  +--------------------+   |   |   |   |   |
+//                 |  |  Xóa phiếu mượn    |<-----------------+
+//                 |  +--------------------+   |   |   |   |
+//                 |                           |   |   |   |
+//                 |  +--------------------+   |   |   |   |
+//                 |  | Hiển thị thông tin |<-----------------+
+//                 |  +--------------------+   |   |   |
+//                 +---------------------------+   |   |
+//                                                |   |
+//                                                |   |
+//                                                |   |
+//                                              +-----+------+
+//                                              |   Sinh viên  |
+//                                              +-----+------+
+//                                                    |
+//                                                    |
+//                                              +-----+------+
+//                                              |   Thủ thư   |
+//                                              +------------+
+//
+//Giải thích sơ đồ Use Case
+//Các thành phần chính:
+//Hệ Thống Thư Viện: Được biểu diễn bởi hộp lớn bao quanh các use case.
+//Use Cases:
+//Đăng ký sinh viên
+//Mượn sách
+//Trả sách
+//Thêm phiếu mượn
+//Xóa phiếu mượn
+//Hiển thị thông tin phiếu mượn
+//Diễn viên (Actors):
+//Sinh viên: Thực hiện các hành động đăng ký, mượn, trả sách.
+//Thủ thư: Thực hiện các hành động thêm, xóa phiếu mượn và hiển thị thông tin phiếu mượn.
+//Mô tả chi tiết:
+//Đăng ký sinh viên: Sinh viên cung cấp thông tin cá nhân để đăng ký làm thành viên của thư viện.
+//Mượn sách: Sinh viên mượn sách từ thư viện, tạo ra một phiếu mượn mới.
+//Trả sách: Sinh viên trả sách đã mượn, hệ thống xóa phiếu mượn tương ứng.
+//Thêm phiếu mượn: Thủ thư thêm phiếu mượn mới vào hệ thống khi sinh viên mượn sách.
+//Xóa phiếu mượn: Thủ thư xóa phiếu mượn khỏi hệ thống khi sinh viên trả sách.
+//Hiển thị thông tin phiếu mượn: Thủ thư xem thông tin của tất cả các phiếu mượn trong hệ thống.
+public class a {
+}
